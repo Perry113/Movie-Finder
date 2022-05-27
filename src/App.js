92e5897bd2b07110eec23519f906e2e020cce04d
Perry113 <mm6mm@wp.pl>
2022-05-27 23:48:42 +0200

    Search component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <header>
       <h1>Movie Database</h1>
     </header>
+    <main>
+      <Search/>
+    </main>
   </div>
   );
 }
---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Search() {
+  return (
+    <section className='searchbox-wrap'>
+        <input type='text' placeholder='Search for a movie...' className='searchbox' />
+    </section>
+  )
+}
+
+export default Search
