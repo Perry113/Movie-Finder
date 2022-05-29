677b4c7a83f0aca41882d679bfaff3a84ab15c66
Perry113 <mm6mm@wp.pl>
2022-05-29 23:58:40 +0200

    app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import Search from './components/Search';
 
 function App() {
+  const [state, setState] = useState({
+    s: '''',
+    results: [], 
+    selected: {}
+  });
+
+  const handleInput = (e) => {
+    let s= e.target.value;
+
+    setState(prevStare => {
+      return s = e.target.value;
+    });
+    console.log(state.s);
+  }
+
   return(
   <div className='App'>
     <header>
